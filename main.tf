7da323052a9fd138f6901fbc096941b3e2c65c53
samidbb <samdi@dfds.com>
2024-03-21 10:51:22 +0100

    minor

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "this" {
   ami                  = data.aws_ami.this.id
   instance_type        = "t3.micro"
   subnet_id            = data.aws_subnet.selected.id
-  iam_instance_profile = "ssm-tunnel"
+  iam_instance_profile = local.aws_iam_instance_profile
   tags = {
     Name = var.name
   }
